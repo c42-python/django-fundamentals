bcdd618666e7a6d15cd4836df22986317e65766b
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-05-03 07:33:36 +0100

    Gameplay app: add and apply migrations for new models

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Game(models.Model):
+    first_player = models.ForeignKey(User, related_name="game_first_player")
+    second_player = models.ForeignKey(User, related_name="game_second_player")
+    start_time = models.DateField(auto_now_add=True)
+    last_active = models.DateField(auto_now=True)
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+
+    # one-to-many relationship between Moves and Game
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
