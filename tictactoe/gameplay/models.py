6394902c62b34487aec126bb1ab554221ec5330d
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-05-03 07:38:21 +0100

    Model update and apply migration

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -7,6 +7,12 @@ class Game(models.Model):
     second_player = models.ForeignKey(User, related_name="game_second_player")
     start_time = models.DateField(auto_now_add=True)
     last_active = models.DateField(auto_now=True)
+    status = models.CharField(max_length=1, default='F') # not specifing default with error when running 'makemigrations'
+    ''' You are trying to add a non-nullable field 'status' to game without a default; we can't do that (the database needs something to populate existing rows).
+        Please select a fix:
+        1) Provide a one-off default now (will be set on all existing rows with a null value for this column)
+        2) Quit, and let me add a default in models.py
+ '''
 
 class Move(models.Model):
     x = models.IntegerField()
